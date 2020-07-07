32b5c046b9f8519d8e4c4f1a876de6a2a69b24e3
Evaldo <evaldocz18@gmail.com>
2020-07-06 21:12:56 -0300

    Class Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import profile from './assets/foto.png';
+// import profile from './assets/foto.png';
+import TechList from './components/TechList';
+
 
 function App() {
-  return <img width="200" src={profile} />
+  return <TechList />
 }
 
 export default App;
